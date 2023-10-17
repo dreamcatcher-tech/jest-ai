6f8b5d3786bba577968f5345d89bbbbf7cef6811
inverted_capital <evil_liskov@protonmail.com>
2023-10-17 14:30:52 +1300

    bin finally fixed

--- bin.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-exec /usr/bin/env NODE_NO_WARNINGS=1 node --loader=import-jsx "$@"
--- cli.js
+++ cli.js
@@ -1,28 +1,8 @@
 #!/bin/sh
-':' //# comment; exec /usr/bin/env node NODE_NO_WARNINGS=1 --loader=import-jsx "$0" "$@"
+':' //;exec /usr/bin/env NODE_NO_WARNINGS=1 node --loader=import-jsx "$0" "$@"
 
-// import React from 'react'
-// import { render } from 'ink'
-// import meow from 'meow'
-// import App from './src/app.js'
+import React from 'react'
+import { render } from 'ink'
+import App from './src/app.js'
 
-console.log('booting...')
-
-// const cli = meow(
-//   `
-// 		Usage
-// 		  $ my-ink-cli
-
-// 		Options
-// 			--name  Your name
-
-// 		Examples
-// 		  $ my-ink-cli --name=Jane
-// 		  Hello, Jane
-// 	`,
-//   {
-//     importMeta: import.meta,
-//   },
-// )
-
-// render(<App />, { exitOnCtrlC: true, patchConsole: true })
+render(<App />, { exitOnCtrlC: true, patchConsole: true })
