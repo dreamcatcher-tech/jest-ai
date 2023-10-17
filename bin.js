3195546369fdc59749d209152ea42b851486ffb3
inverted_capital <evil_liskov@protonmail.com>
2023-10-17 19:43:52 +1300

    minor

--- bin.js
+++ bin.js
@@ -3,7 +3,7 @@
 import * as url from 'url'
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
 import spawn from 'cross-spawn'
-const args = ['--loader=import-jsx', __dirname + 'src/cli.js']
+const args = ['--loader', 'import-jsx', __dirname + 'src/cli.js']
 
 spawn('node', args, {
   stdio: 'inherit',
